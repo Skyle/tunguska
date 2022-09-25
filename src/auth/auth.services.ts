5377efa3387691a95ba1cdbdbc51597fbb5601d9
Philipp Friedrich <freezer05@googlemail.com>
2022-09-25 09:08:37 +0200

    refactor tokenToUser

--- src/auth/auth.services.ts
+++ src/auth/auth.services.ts
@@ -12,7 +12,28 @@ async function updateLastVisited(user: UserDB) {
 
 export async function verifyUserOrThrow(ctx: MercuriusContext) {
   const jwt = ctx.authorization?.split(" ")[1];
-  if (!jwt) throw new Error("UNAUTHORIZED");
+  if (jwt) {
+    return await tokenToUser(jwt, ctx);
+  } else {
+    throw new Error("UNAUTHORIZED");
+  }
+}
+
+export async function verifyUser(
+  ctx: MercuriusContext
+): Promise<UserDB | null> {
+  const jwt = ctx.authorization?.split(" ")[1];
+  if (jwt) {
+    return await tokenToUser(jwt, ctx);
+  } else {
+    return null;
+  }
+}
+
+export async function tokenToUser(
+  jwt: string,
+  ctx: MercuriusContext
+): Promise<UserDB> {
   try {
     const verifiedJWT: FastifyJWT["payload"] = ctx.app.jwt.verify(jwt);
     if (verifiedJWT.sub) {
@@ -28,34 +49,5 @@ export async function verifyUserOrThrow(ctx: MercuriusContext) {
     }
   } catch (error) {
     throw new Error("UNAUTHORIZED");
-  }
-}
-
-export async function verifyUser(
-  ctx: MercuriusContext
-): Promise<UserDB | null> {
-  const jwt = ctx.authorization?.split(" ")[1];
-  if (!jwt) {
-    return null;
-  } else {
-    try {
-      const verifiedJWT: FastifyJWT["payload"] = ctx.app.jwt.verify(jwt);
-      if (verifiedJWT.sub) {
-        const user = await prisma.userDB.findUnique({
-          where: { id: verifiedJWT.sub },
-        });
-        if (!user) throw new Error("UNAUTHORIZED");
-        await prisma.userDB.update({
-          where: { id: user.id },
-          data: { lastVisitedAt: new Date() },
-        });
-        ctx.user = user;
-        return user;
-      } else {
-        throw new Error("UNAUTHORIZED");
-      }
-    } catch (error) {
-      throw new Error("UNAUTHORIZED");
-    }
   }
 }
