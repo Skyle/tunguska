e7d1f77b6a166e8d72a3d177f5640fd534fb3d90
Philipp Friedrich <freezer05@googlemail.com>
2022-09-25 11:25:54 +0200

    connect with user

--- src/image/image.resolvers.ts
+++ src/image/image.resolvers.ts
@@ -2,22 +2,28 @@ import { createWriteStream } from "fs";
 import path from "path";
 import { pipeline } from "stream/promises";
 import { prisma } from "..";
+import { verifyUserOrThrow } from "../auth/auth.services";
 import { MutationResolvers } from "../graphql/generated";
 
 const uploadsDir = path.resolve("./files/images");
 
 export const uploadImage: MutationResolvers["uploadImage"] = async (
   root,
-  { image }
+  { image },
+  ctx
 ) => {
   try {
     console.log(image);
+    const verifiedUser = await verifyUserOrThrow(ctx);
+
     const { file } = await image;
     const { filename, createReadStream } = file;
     const rs = createReadStream();
     const ws = createWriteStream(path.join(uploadsDir, filename));
     await pipeline(rs, ws);
-    const newImage = await prisma.imageDB.create({ data: {} });
+    const newImage = await prisma.imageDB.create({
+      data: { createdBy: { connect: { id: verifiedUser.id } } },
+    });
     console.log("newImage", newImage);
     return "lol";
   } catch (error) {
